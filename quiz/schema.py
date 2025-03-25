6ec2e08b79e741332b2ec5d42a971e160b1121c7
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-25 18:04:17 +0100

    first implementation for the schema

--- quiz/schema.py
+++ quiz/schema.py
@@ -1,1 +1,35 @@
-schema = None
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Category, Quiz, Question, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class QuizType(DjangoObjectType):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+
+class Query(graphene.ObjectType):
+
+    quiz = graphene.String()
+
+    def resolve_quiz(root, info):
+        return f'This is the first question'
+
+
+schema = graphene.Schema(query=Query)
