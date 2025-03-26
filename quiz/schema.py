cee1b23ed23a89fa89a33b186ad44b8f22abd344
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-26 11:17:57 +0100

    get question by id and get question answers by question id

--- quiz/schema.py
+++ quiz/schema.py
@@ -32,12 +32,24 @@ class Query(graphene.ObjectType):
 
     quiz_by_id = graphene.Field(QuizType, id=graphene.UUID())
     
+    question_by_id = graphene.Field(QuestionType, id=graphene.UUID())
+
+    answers_by_question_id = graphene.List(AnswerType, id=graphene.UUID())
+
+
+    
     def resolve_quiz_by_id(root, info, id):
         return Quiz.objects.get(id=id)
     
     def resolve_all_questions(root, info):
         return Question.objects.all()
    
+    def resolve_question_by_id(root, info, id):
+        return Question.objects.get(id=id)
+    
+    def resolve_answers_by_question_id(root, info, id):
+        print(id)
+        return Answer.objects.filter(question__id=id)
 
 
 schema = graphene.Schema(query=Query)
